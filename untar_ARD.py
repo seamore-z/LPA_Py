08bf1c42feaa5fa44d2c4c9c49cd35a0aea7df1c
Xiaoran Zhu <seamorez@scc-dd4.scc.bu.edu>
2023-05-30 19:26:02 -0400

    Add existing file

--- untar_ARD.py
+++ untar_ARD.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 ## -----------------------------------------------------------------------------------------------------------------##
 ## Written by Eli Melaas, adapted for Python by Seamore Zhu 
 ## -----------------------------------------------------------------------------------------------------------------##
-
-
-# In[1]:
-
 
 from joblib import Parallel, delayed
 import sys
@@ -18,16 +8,8 @@ import os
 import glob
 import tarfile
 
-
-# In[2]:
-
-
 # Register parallel processing with joblib
 Parallel(n_jobs=16)
-
-
-# In[3]:
-
 
 #args = sys.argv[1:]  # Get command-line arguments
 #tile = args[0]       # Extract the first argument to tile name
@@ -35,8 +17,6 @@ Parallel(n_jobs=16)
 
 tile = 'h15v02'  # Will be parameterized in json file and passed as argument using shell script (see above code)
 data_dir = '/projectnb/modislc/users/seamorez/Landsat_Pheno/ARD/'  # Will be parameterized in json file
-
-#os.getcwd() #just to check what folder is currently working directory
 
 #os.chdir(data_dir+tile)
 os.chdir(data_dir+tile) 
@@ -48,25 +28,13 @@ os.makedirs(os.getcwd()+'/NLCD/', exist_ok=True)
 os.makedirs(os.getcwd()+'/PHENO/', exist_ok=True)
 os.makedirs(os.getcwd()+'/SHP/', exist_ok=True)
 
-
-# In[10]:
-
-
 # Untar all inputs and sort into folders
 in_dirs_SR = glob.glob("*SR*", recursive=True)
 in_dirs_TA = glob.glob("*TOA*", recursive=True)
 
-
-# In[12]:
-
-
 dir_names = [os.path.basename(dir_SR)[0:32] for dir_SR in in_dirs_SR]
 for dir_name in dir_names:
     os.makedirs(os.path.join(os.getcwd(), 'IMG', dir_name), exist_ok=True)
-
-
-# In[34]:
-
 
 for i in range(len(in_dirs_TA)):
     print(i)
@@ -80,10 +48,6 @@ for i in range(len(in_dirs_TA)):
     for file in in_dirs_TAB:
         os.remove(file)
 
-
-# In[27]:
-
-
 for i in range(len(in_dirs_SR)):
     print(i)
 
@@ -92,10 +56,3 @@ for i in range(len(in_dirs_SR)):
     tar.extractall(os.path.join(os.getcwd(), 'IMG', dir_name_SR))
     tar.close()
     os.remove(in_dirs_SR[i])
-
-
-# In[ ]:
-
-
-
-
